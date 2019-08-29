e0e7181abdb9a83b488c3283ff392b5a7c186be5
Suraj Dubey <surajkd050599@gmail.com>
2019-08-29 18:14:15 +0530

    str to list

--- wordcount/views.py
+++ wordcount/views.py
@@ -10,16 +10,15 @@ def count(request):
     wordlist = fulltext.split()
     countword = dict(Counter(wordlist))
     m = 0
-    w = ""
-    d = ""
+    d = []
     for i,v in countword.items():
         if(v > m):
             m = v
-            w = i
+            d.append(i)
         elif(v == m):
             m = v
-            d = i
-    return render(request,"count.html",{"fulltext":fulltext,"count":len(wordlist),"maximum":(w,d), "words":countword})
+            d.append(i)
+    return render(request,"count.html",{"fulltext":fulltext,"count":len(wordlist),"maximum":d, "words":countword})
 
 def about(request):
     return render(request,"about.html")
